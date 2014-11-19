3bbbd454ffa4201aa4fc5949324fc3e4a76d0f39
Clayton Wallace <clayto1987@gmail.com>
2014-11-19 08:29:48 -0600

    Completed parsing html to view article in-app

--- app/src/main/java/com/example/clayto/rssreaderapp/DetailsActivity.java
+++ app/src/main/java/com/example/clayto/rssreaderapp/DetailsActivity.java
@@ -1,9 +1,11 @@
 package com.example.clayto.rssreaderapp;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.Html;
@@ -13,10 +15,18 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class DetailsActivity extends Activity {
 
-    private String title, publishDate, description, link, fontSizePref, themePref;
-    private TextView titleTextView, dateTextView, descriptionTextView, linkTextView;
+    private String title, author, publishDate, description, link, fontSizePref, themePref;
+    private TextView titleTextView, authorTextView, dateTextView, descriptionTextView, linkTextView;
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +40,20 @@ public class DetailsActivity extends Activity {
             int arrayListPosition = intent.getIntExtra(RSSFeedsFragment.SELECTED_ARRAY_POSITION,0);
             title = intent.getStringArrayListExtra(RSSFeedsFragment.TITLES_ARRAYLIST).get(arrayListPosition);
             publishDate = intent.getStringArrayListExtra(RSSFeedsFragment.PUBLISH_DATES_ARRAYLIST).get(arrayListPosition);
-            description = intent.getStringArrayListExtra(RSSFeedsFragment.DESCRIPTIONS_ARRAYLIST).get(arrayListPosition);
+            //description = intent.getStringArrayListExtra(RSSFeedsFragment.DESCRIPTIONS_ARRAYLIST).get(arrayListPosition);
             link = intent.getStringArrayListExtra(RSSFeedsFragment.LINKS_ARRAYLIST).get(arrayListPosition);
 
             String htmlLink = "<a href=\"" + link + "\"> Read More</a>";
 
             titleTextView = (TextView)findViewById(R.id.title);
+            authorTextView = (TextView)findViewById(R.id.author);
             dateTextView = (TextView)findViewById(R.id.publish_date);
             descriptionTextView = (TextView)findViewById(R.id.description);
             linkTextView = (TextView)findViewById(R.id.link);
 
             titleTextView.setText(Html.fromHtml(title));
             dateTextView.setText(Html.fromHtml(publishDate));
-            descriptionTextView.setText(Html.fromHtml(description));
+            //descriptionTextView.setText(Html.fromHtml(description));
             linkTextView.setText(Html.fromHtml(htmlLink));
             linkTextView.setMovementMethod(LinkMovementMethod.getInstance());
 
@@ -63,12 +74,14 @@ public class DetailsActivity extends Activity {
         if (themePref.equals("Dark")) {
             findViewById(R.id.details_activity_layout).setBackgroundColor(Color.BLACK);
             titleTextView.setTextColor(Color.WHITE);
+            authorTextView.setTextColor(Color.WHITE);
             dateTextView.setTextColor(Color.WHITE);
             descriptionTextView.setTextColor(Color.WHITE);
             linkTextView.setTextColor(Color.WHITE);
         } else {
             findViewById(R.id.details_activity_layout).setBackgroundColor(Color.WHITE);
             titleTextView.setTextColor(Color.BLACK);
+            authorTextView.setTextColor(Color.BLACK);
             dateTextView.setTextColor(Color.BLACK);
             descriptionTextView.setTextColor(Color.BLACK);
             linkTextView.setTextColor(Color.BLACK);
@@ -76,20 +89,25 @@ public class DetailsActivity extends Activity {
 
         if(fontSizePref.toLowerCase().equals("small")) {
             titleTextView.setTextSize(10);
+            authorTextView.setTextSize(8);
             dateTextView.setTextSize(8);
             descriptionTextView.setTextSize(8);
             linkTextView.setTextSize(8);
         } else if(fontSizePref.toLowerCase().equals("large")) {
             titleTextView.setTextSize(27);
+            authorTextView.setTextSize(24);
             dateTextView.setTextSize(24);
             descriptionTextView.setTextSize(24);
             linkTextView.setTextSize(24);
         } else {
             titleTextView.setTextSize(18);
+            authorTextView.setTextSize(16);
             dateTextView.setTextSize(16);
             descriptionTextView.setTextSize(16);
             linkTextView.setTextSize(16);
         }
+
+        new ParseHTML().execute();
     }
 
     @Override
@@ -105,14 +123,12 @@ public class DetailsActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()) {
-            case R.id.action_view_article_offline:
-                return true;
             case R.id.action_download_article:
-                Intent parseArticleIntent = new Intent(DetailsActivity.this,FullArticleActivity.class);
-                parseArticleIntent.putExtra(getResources().getString(R.string.article_title),title);
-                parseArticleIntent.putExtra(getResources().getString(R.string.article_publish_date),publishDate);
-                parseArticleIntent.putExtra(getResources().getString(R.string.article_url_link),link);
-                startActivity(parseArticleIntent);
+//                Intent parseArticleIntent = new Intent(DetailsActivity.this,FullArticleActivity.class);
+//                parseArticleIntent.putExtra(getResources().getString(R.string.article_title),title);
+//                parseArticleIntent.putExtra(getResources().getString(R.string.article_publish_date),publishDate);
+//                parseArticleIntent.putExtra(getResources().getString(R.string.article_url_link),link);
+//                startActivity(parseArticleIntent);
                 return true;
             case R.id.action_settings:
                 Intent settingsIntent = new Intent(DetailsActivity.this,SettingsActivity.class);
@@ -122,5 +138,52 @@ public class DetailsActivity extends Activity {
                 return super.onOptionsItemSelected(item);
         }
 
+    }
+
+    private class ParseHTML extends AsyncTask<Void, Void, Void> {
+
+        String author;
+        String content;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(DetailsActivity.this);
+            mProgressDialog.setTitle("RSS Reader");
+            mProgressDialog.setMessage("Gathering Article...");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                // Connect to the web site
+                Document document = Jsoup.connect(link).userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 Safari/537.36").get();
+                // Get the html document title
+                Element htmlBody = document.body();
+                Elements mainContent = htmlBody.select("#main_column");
+
+                //Elements titleContent = mainContent.select(".top_head");
+                Elements authorContent = mainContent.select(".first_byline");
+                Elements articleContent = mainContent.select(".article > p");
+
+                //title = titleContent.get(0).text();
+                content = articleContent.text();
+                author = authorContent.get(0).text();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            // Set title into TextView
+            authorTextView.setText(this.author);
+            descriptionTextView.setText(this.content);
+            mProgressDialog.dismiss();
+        }
     }
 }
